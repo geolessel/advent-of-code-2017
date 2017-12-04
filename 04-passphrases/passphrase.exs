a3ca0206c95976bf039cf79cf00ba0e81e0b9601
Geoffrey Lessel <geo@geoffreylessel.com>
2017-12-04 10:59:05 -0800

    Day 4 step 2

--- 04-passphrases/passphrase.exs
+++ 04-passphrases/passphrase.exs
@@ -7,6 +7,12 @@ defmodule Passphrase do
     |> Enum.reduce(0, &(if valid?(&1, :step1), do: &2 + 1, else: &2))
   end
 
+  def step2(phrases \\ @input) do
+    phrases
+    |> String.split("\n")
+    |> Enum.reduce(0, &(if valid?(&1, :step2), do: &2 + 1, else: &2))
+  end
+
   def valid?(phrase, :step1) do
     phrase
     |> String.split()
@@ -16,6 +22,28 @@ defmodule Passphrase do
     end)
     |> Kernel.==(0)
   end
+
+  def valid?(phrase, :step2) do
+    phrases =
+      phrase
+      |> String.split()
+      |> Enum.map(fn(phrase) ->
+        phrase
+        |> String.split("", trim: true)
+        |> Enum.sort()
+      end)
+
+    phrases
+    |> Enum.reduce(0, fn(phrase, acc) ->
+      if Enum.count(phrases, &(&1 == phrase)) > 1 do
+        acc + 1
+      else
+        acc
+      end
+    end)
+    |> Kernel.==(0)
+  end
 end
 
 Passphrase.step1() |> IO.inspect(label: "step1 valid passphrases")
+Passphrase.step2() |> IO.inspect(label: "step2 valid passphrases")
--- 04-passphrases/passphrase_text.exs
+++ 04-passphrases/passphrase_text.exs
@@ -16,4 +16,24 @@ defmodule PassphraseTest do
   test "step1 aa bb cc dd aaa is valid" do
     assert Passphrase.valid?("aa bb cc dd aaa", :step1)
   end
+
+  test "step2 abcde fghij is valid" do
+    assert Passphrase.valid?("abcde fghij", :step2)
+  end
+
+  test "step2 abcde xyz ecdab is not valid" do
+    refute Passphrase.valid?("abcde xyz ecdab", :step2)
+  end
+
+  test "step2 a ab abc abd abf abj is valid" do
+    assert Passphrase.valid?("a ab abc abd abf abj", :step2)
+  end
+
+  test "step2 iiii oiii ooii oooi oooo is valid" do
+    assert Passphrase.valid?("iiii oiii ooii oooi oooo", :step2)
+  end
+
+  test "step2 oiii ioii iioi iiio is not valid" do
+    refute Passphrase.valid?("oiii ioii iioi iiio", :step2)
+  end
 end
